d6297343bd4625bde31f01859c678314ed42636f
caniza1es <88113151+caniza1es@users.noreply.github.com>
2022-04-26 12:37:18 -0500

    Update and rename LIBRERIAPAPELERIAplpg.sql to LIBRERIAPAPELERIA.sql

--- LIBRERIAPAPELERIA.sql
+++ LIBRERIAPAPELERIA.sql
@@ -1,12 +1,14 @@
 CREATE TABLE Branches(
 name varchar(25) PRIMARY KEY,
 address VARCHAR(25) NOT NULL,
-budget INTEGER NOT NULL
+budget INTEGER DEFAULT 20000000 CHECK(budget > 0)
 );
+
 CREATE TABLE Occupations(
 description VARCHAR(20) PRIMARY KEY,
-salary integer NOT NULL
+salary integer NOT NULL CHECK(salary > 0)
 );
+
 CREATE TABLE Employees(
 CC integer PRIMARY KEY,
 name VARCHAR(40) NOT NULL,
@@ -18,13 +20,15 @@ email VARCHAR(50)
 
 CREATE TABLE Products(
 ID INTEGER PRIMARY KEY,
-des varchar(30) NOT NULL
+des varchar(30) NOT NULL,
+price INTEGER CHECK(price > price_bought),
+price_bought INTEGER CHECK(price_bought > 0)
 );	
 
 CREATE TABLE Inventories(
 branch VARCHAR(25) REFERENCES Branches(name),
 product INTEGER REFERENCES Products(ID),
-amount INTEGER NOT NUll,
+amount INTEGER CHECK(amount > 0) DEFAULT 0,
 PRIMARY KEY(branch,product)
 );
 
@@ -33,10 +37,17 @@ id INTEGER PRIMARY KEY,
 name VARCHAR(50) NOT NULL,
 email TEXT NULL
 );
+
+CREATE TABLE Editorials(
+ID INTEGER PRIMARY KEY,
+NAME VARCHAR(50) NOT NULL,
+contacto TEXT NOT NULL
+);
+
 CREATE TABLE Books(
 id integer REFERENCES Products(ID),
 title VARCHAR(50) NOT NULL,
-editorial VARCHAR(40),
+editorial INTEGER REFERENCES Editorials(ID),
 author INTEGER REFERENCES Authors(ID),
 pub_date DATE NOT NULL,
 genre VARCHAR(30) NOT NULL,
@@ -56,43 +67,13 @@ time TIMESTAMP NOT NULL,
 client INTEGER REFERENCES Clients(CC) NOT NULL,
 employee INTEGER REFERENCES employees(CC) NOT NULL,
 branch varchar(25) REFERENCES Branches(name)
+ON DELETE CASCADE
 );
 
 CREATE TABLE Receipts_desc(
 ID INTEGER REFERENCES Receipts(ID),
 product INTEGER REFERENCES Products(Id),
-amount INTEGER NOT NULL,
+amount INTEGER NOT NULL CHECK(amount > 0),
 PRIMARY KEY(id,product)
 );
 
-INSERT INTO branches(name,address,budget)
-	VALUES('Norte','Autopista Nte #168-30',5000000);
-    
-INSERT INTO occupations(description,salary)
-	VALUES('Caja',2500000);
-    
-INSERT INTO employees(cc,name,branch,occupation,working_since,email)
-	VALUES(1000518517,'Juan Alvarez','Norte','Caja','2019-01-01','juanalavarez@hotmail.com');
-    
-INSERT INTO products(id,des)
-	VALUES(0001,'Micropunta'),
-   		  (0002,'ejericios matematica');
-          
-INSERT INTo inventories(branch,product,amount)
-	VALUES('Norte',0001,300),
-     	  ('Norte',0002,5);
-INSERT INTo authors(id,name)
-	VALUES(2222,'Johann Mastropiero');
-    
-INSERT INTO books(id,title,editorial,author,pub_date,genre)
-	VALUES(0002,'Acertijos','Letras rojas',2222,'2000-01-01','Ciencia');
-    
-INSERT INTO clients(cc,name,email,phone)
-	VALUES(100323678,'Juan Canizales','juan.canizales@urosario.edu.co',3136470503);
-    
-INSERT INTO receipts(id,time,client,employee,branch)
-	VALUES(5000,'2020-06-22 19:10:25-07',100323678,1000518517,'Norte');
-
-INSERT INTO receipts_desc(ID,product,amount)
-	VALUES(5000,0001,3),
-    	 (5000,0002,1);
