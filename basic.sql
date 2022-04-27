f543bab19f9b98891c1aab6d5c5ffe7adf71eeca
caniza1es <88113151+caniza1es@users.noreply.github.com>
2022-04-27 09:01:40 -0500

    Update basic.sql

--- basic.sql
+++ basic.sql
@@ -3,9 +3,17 @@ SELECT * FROM employees
 ORDER by branch
 );
 
-create view total_inventories AS (
+CREATE OR REPLACE view total_inventories AS (
 SELECT P.des, P.id,SUM(I.amount) as TOTAl
 FROM products as P, inventories as I
 WHERE P.id = I.product
 GROUP BY P.des,P.id
 )
+
+CREATE VIEW total_books AS (
+SELECT B.title, B.editorial,B.author,B.genre,SUM(I.amount) as total
+FROM books as B, products as P, inventories as I
+WHERE B.id = p.id
+AND I.product = P.id
+GROUP BY B.title,B.editorial,B.author,B.genre
+);
